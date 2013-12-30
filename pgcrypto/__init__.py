0afc9a87809247b582567144635852148ffe0a20
maurycyp <github.com@wayheavy.com>
2013-12-30 14:50:17 -0500

    Import models.Field subclasses into __init__.py

--- pgcrypto/__init__.py
+++ pgcrypto/__init__.py
@@ -15,4 +15,7 @@
 __version_info__ = (1, 1, 0)
 __version__ = '.'.join(str(i) for i in __version_info__)
 
+from .fields import (EncryptedCharField, EncryptedDecimalField,
+                     EncryptedDateField, EncryptedDateTimeField,
+                     EncryptedEmailField, EncryptedTextField)
 from .utils import pad, unpad, armor, dearmor, aes_pad_key
